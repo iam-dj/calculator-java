b23cc0a490d5161342bcadef0d8e706372ce0f40
iam-dj <6887240+iam-dj@users.noreply.github.com>
2023-07-12 16:58:11 -0700

    created a simple calculator in Java

---
+++ Calculator.java
@@ -0,0 +1,38 @@
+public class Calculator {
+
+  public void myCalculator() {}
+
+  public int add(int a, int b) {
+    int result = a + b;
+    return result;
+  }
+
+  public int subtract(int a, int b) {
+    int result = a - b;
+    return result;
+  }
+
+  public int multiply(int a, int b) {
+    int product = a * b;
+    return product;
+  }
+
+  public int divide(int a, int b) {
+    int result = a / b;
+    return result;
+  }
+
+  public int modulo(int a, int b) {
+    int result = a % b;
+    return result;
+  }
+
+  public static void main(String[] args) {
+    Calculator myCalculator = new Calculator();
+    System.out.println(myCalculator.add(5, 7));
+    System.out.println(myCalculator.subtract(45, 11));
+    System.out.println(myCalculator.multiply(45, 11));
+    System.out.println(myCalculator.divide(45, 11));
+    System.out.println(myCalculator.modulo(24, 11));
+  }
+}
